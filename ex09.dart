5a8eb887290cbe5b17bc49e1ae87d6db2c79322b
Luis <86859569+lsstomas@users.noreply.github.com>
2023-02-24 10:01:08 -0300

    Exercício 09

---
+++ ex09.dart
@@ -0,0 +1,9 @@
+// Exercício 09 - Write a program in Dart to remove all whitespaces from String.
+
+void main() {
+  String frase = '   Essa  é uma    frase  com   diversos espaços.  ';
+ 
+  resultado = frase.replaceAll(' ', '');
+
+  print(resultado);
+}
